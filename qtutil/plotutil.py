4e81dccce07da7a68e421cf9078ed637149f616e
pcx <p_lord@163.com>
2023-03-16 01:25:45 +0800

    更改了qtutil

--- qtutil/plotutil.py
+++ qtutil/plotutil.py
@@ -8,5 +8,17 @@ import matplotlib.pyplot as plt
 
 
 class MyPlot(FigureCanvas):
-    def __init__(self):
-        pass
+    def __init__(self,
+                 nrow: int,
+                 ncol: int,
+                 parent=None,
+                 width: int = 15,
+                 height: int = 10,
+                 dpi: int = 100,
+                 bg_color: str = "white"):
+        # 设置支持中文
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        # 设置-号
+        plt.rcParams['axes.unicode_minus'] = False
+
+
